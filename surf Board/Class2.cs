70a16d1adcf9359db2efefcf2af6bdce05e1bc0c
Prasadini Imansika <rathnay-ec22027@stu.kln.ac.lk>
2026-06-17 10:49:10 +0530

    rental form editing

---
+++ surf Board/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace surf_Board
+{
+    public class Rental
+    {
+        public string RentalID { get; set; }
+        public string CustomerID { get; set; }
+        public string Surfboard { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public int Hours { get; set; }
+        public decimal PricePerHour { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+    }
+
+}
--- surf Board/Program.cs
+++ surf Board/Program.cs
@@ -11,7 +11,13 @@ namespace surf_Board
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+<<<<<<< Updated upstream
             Application.Run(new Form1());
+=======
+
+            Application.Run(new Rental_Management());
+           
+>>>>>>> Stashed changes
         }
     }
 }
