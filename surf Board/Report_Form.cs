32fab7912ea12ec6e608d6849502fa832d4e2395
jr123-abc <jayanirandi2020@gmail.com>
2026-06-19 21:19:48 +0530

    completed

--- surf Board/Report_Form.cs
+++ surf Board/Report_Form.cs
@@ -7,7 +7,7 @@ namespace surf_Board
 {
     public partial class Report_Form : Form
     {
-        // Oyage Booking form eke thiyena connection string ekama damma machan
+        
         MySqlConnection conn = new MySqlConnection("server=localhost;database=SurfSchoolDB;uid=root;pwd=;");
 
         public Report_Form()
@@ -15,7 +15,7 @@ namespace surf_Board
             InitializeComponent();
         }
 
-        // Form load weddi select box eka default thiyaganna
+        
         private void Report_Form_Load(object sender, EventArgs e)
         {
             cmbReportType.SelectedIndex = -1;
@@ -23,10 +23,10 @@ namespace surf_Board
             dtpTo.Value = DateTime.Now;
         }
 
-        // GENERATE REPORT BUTTON CLICK
+        
         private void btnGenerateReport_Click(object sender, EventArgs e)
         {
-            // Validation: ComboBox eka select karala nathnam warning ekak denawa
+            
             if (cmbReportType.SelectedIndex == -1)
             {
                 MessageBox.Show("Please select a report type first!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -36,32 +36,32 @@ namespace surf_Board
             string selectedReport = cmbReportType.SelectedItem.ToString();
             string query = "";
 
-            // Oyage database tables scene ekata galapenna queries tika
+            
             if (selectedReport == "Booking Report")
             {
                 query = "SELECT * FROM Bookings WHERE BookingDate BETWEEN @FromDate AND @ToDate";
             }
             else if (selectedReport == "Payment Report")
             {
-                // Payment date field eka database eke thiyena widiyata check karanna machan
+                
                 query = "SELECT * FROM Payments WHERE PaymentDate BETWEEN @FromDate AND @ToDate";
             }
             else if (selectedReport == "Customer Report")
             {
-                // Customer form eke date ekak track wenawa nam e field eka methana danna
+                
                 query = "SELECT * FROM Customers WHERE RegisteredDate BETWEEN @FromDate AND @ToDate";
             }
 
-            // Data load karana method eka call කරනවා
+            
             LoadReportData(query);
         }
 
-        // DATABASE DATA LOAD METHOD
+        
         private void LoadReportData(string sqlQuery)
         {
             try
             {
-                // Database connection open kirima safely
+                
                 if (conn.State == ConnectionState.Closed)
                 {
                     conn.Open();
@@ -69,7 +69,7 @@ namespace surf_Board
 
                 MySqlCommand cmd = new MySqlCommand(sqlQuery, conn);
 
-                // DateTimePicker walin select karana dates deka parameters widiyata pass karanawa
+                
                 cmd.Parameters.AddWithValue("@FromDate", dtpFrom.Value.Date);
                 cmd.Parameters.AddWithValue("@ToDate", dtpTo.Value.Date);
 
@@ -77,10 +77,10 @@ namespace surf_Board
                 DataTable dt = new DataTable();
                 da.Fill(dt);
 
-                // Oyage DataGridView (dgvReports) ekata data set kirima
+                
                 dgvReports.DataSource = dt;
 
-                // Data mukuth nathnam user ta message ekak denawa
+                
                 if (dt.Rows.Count == 0)
                 {
                     MessageBox.Show("No records found for the selected date range.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -92,7 +92,7 @@ namespace surf_Board
             }
             finally
             {
-                // Connection eka aniwarenma close karanawa oyage parana code eke wagema
+                
                 if (conn.State == ConnectionState.Open)
                 {
                     conn.Close();
@@ -100,7 +100,7 @@ namespace surf_Board
             }
         }
 
-        // CLEAR BUTTON (Optional but good to have)
+        
         private void btnClear_Click(object sender, EventArgs e)
         {
             cmbReportType.SelectedIndex = -1;
