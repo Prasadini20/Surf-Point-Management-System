7437ae056f2127a753e652b6dcbd57b1a252f9be
Prasadini Imansika <rathnay-ec22027@stu.kln.ac.lk>
2026-06-21 01:45:40 +0530

    database name  changing

--- surf Board/DBConnection.cs
+++ surf Board/DBConnection.cs
@@ -8,7 +8,7 @@ namespace surf_Board
    
         class DBConnection
         {
-            private static string connectionString = "server=localhost;user=root;password=;database=AquaRideDB";
+            private static string connectionString = "server=localhost;user=root;password=;database=aquaridedb";
 
             public static MySqlConnection GetConnection()
             {
