00547528178f0635000b303207efc5e2033cff1b
CHAMARINDI S.H.K <chamari-ec22025@stu.kln.ac.lk>
2026-06-20 23:28:03 +0530

    updated

---
+++ surf Board/CustomerInstructorView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace surf_Board
+{
+    public partial class CustomerInstructorView : Form
+    {
+        public CustomerInstructorView()
+        {
+            InitializeComponent();
+        }
+
+        private void flpInstructors_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void CustomerInstructorView_Load(object sender, EventArgs e)
+        {
+            InstructorCard card1 = new InstructorCard();
+
+            card1.SetInstructorDetails(
+                "John Silva",
+                "5",
+                "Available"
+            );
+
+            flpInstructors.Controls.Add(card1);
+        }
+    }
+}
---
+++ surf Board/InstructorCard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace surf_Board
+{
+    public partial class InstructorCard : UserControl
+    {
+        public InstructorCard()
+        {
+            InitializeComponent();
+        }
+            public void SetInstructorDetails(string name, string experience, string status)
+        {
+            lblInstructorName.Text = name;
+            lblExperience.Text = "Experience : " + experience + " Years";
+            lblStatus.Text = status;
+        }
+    
+    }
+}
